43e37ab4090663f6157defdd7c365200746b10e1
John Keech <jokeech@microsoft.com>
2023-11-19 00:15:45 +0000

    Scaffolding for game

--- src/game.ts
+++ src/game.ts
@@ -1,47 +1,74 @@
-import * as Phaser from 'phaser';
+import * as Phaser from "phaser";
 
-export default class Demo extends Phaser.Scene
-{
-    constructor ()
-    {
-        super('demo');
+export default class Demo extends Phaser.Scene {
+  private dog: Phaser.Physics.Arcade.Sprite;
+  private background: Phaser.GameObjects.TileSprite;
+
+  constructor() {
+    super("demo");
+  }
+
+  preload() {
+    this.load.image("background", "assets/background.jpeg");
+    this.load.image("dog", "assets/dog.png");
+  }
+
+  create() {
+    this.background = this.add.tileSprite(
+      0,
+      0,
+      this.scale.width,
+      this.scale.height,
+      "background"
+    );
+
+    this.background.setOrigin(0, 0).setScrollFactor(0);
+
+    this.dog = this.physics.add
+      .sprite(this.scale.width / 3, 650, "dog")
+      .setDisplaySize(90, 90)
+      .setCollideWorldBounds(true)
+      .setBounce(0.2);
+
+    // this.cameras.main
+    //   .setBounds(0, 0, 1021 * 2, 716)
+    //   .set
+    //   .setZoom(1)
+    //   .setName("main")
+    //   .startFollow(this.dog, true, 0.5, 0.5);
+  }
+
+  update() {
+    const cursors = this.input.keyboard.createCursorKeys();
+    if (cursors.left.isDown) {
+      //this.dog.setVelocityX(-160);
+      this.background.tilePositionX -= 6;
+    } else if (cursors.right.isDown) {
+      //this.dog.setVelocityX(160);
+      this.background.tilePositionX += 6;
+    } else {
+      this.dog.setVelocityX(0);
     }
 
-    preload ()
-    {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+    if (cursors.up.isDown && this.dog.body.blocked.down) {
+      this.dog.setVelocityY(-330);
     }
-
-    create ()
-    {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
-
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-
-        this.add.image(400, 300, 'libs');
-
-        const logo = this.add.image(400, 70, 'logo');
-
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
-    }
+  }
 }
 
 const config = {
-    type: Phaser.AUTO,
-    backgroundColor: '#125555',
-    width: 800,
-    height: 600,
-    scene: Demo
+  type: Phaser.AUTO,
+  backgroundColor: "#000000",
+  width: 1021,
+  height: 716,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 800 },
+      debug: false,
+    },
+  },
+  scene: Demo,
 };
 
 const game = new Phaser.Game(config);
